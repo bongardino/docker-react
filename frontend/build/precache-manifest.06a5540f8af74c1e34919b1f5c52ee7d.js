f98a913d647eae9ed9c4c3659a0f874145378f2d
Bongardino <bongardino@gmail.com>
2019-01-20 14:33:43 -0500

    initial commit

---
+++ frontend/build/precache-manifest.06a5540f8af74c1e34919b1f5c52ee7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f602cc07bdb6051ae40a",
+    "url": "/static/css/main.30763293.chunk.css"
+  },
+  {
+    "revision": "f602cc07bdb6051ae40a",
+    "url": "/static/js/main.f602cc07.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0eaffff4b0fff109d7d8a07976efb0f8",
+    "url": "/index.html"
+  }
+];
